55ea8b85d0d65f98e2cf9d4c37031802fd8d4185
dmblackman <declan.blackman1@gmail.com>
2021-08-17 11:44:54 -0600

    Initial project build and commit. closes #1

---
+++ Chuck-Norris-Jokes-v1/src/main/java/com/dmb/ChuckNorrisJokesv1/ChuckNorrisJokesV1Application.java
@@ -0,0 +1,13 @@
+package com.dmb.ChuckNorrisJokesv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokesV1Application.class, args);
+	}
+
+}
---
+++ Chuck-Norris-Jokes-v1/src/test/java/com/dmb/ChuckNorrisJokesv1/ChuckNorrisJokesV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmb.ChuckNorrisJokesv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokesV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
